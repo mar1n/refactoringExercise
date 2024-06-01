52d5531f64336a078d00b4cfc66f8ff9df24070b
mar1n <30738216+mar1n@users.noreply.github.com>
2024-06-01 15:24:35 +0100

    load project

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import "vitest/config"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.js',
+  }
+})
